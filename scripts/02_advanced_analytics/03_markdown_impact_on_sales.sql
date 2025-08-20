e93c633dcc3bf9c8e50d21fb6fb7695770f9b645
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:06:29 +0100

    Update 03_markdown_impact_on_sales.sql

--- scripts/02_advanced_analytics/03_markdown_impact_on_sales.sql
+++ scripts/02_advanced_analytics/03_markdown_impact_on_sales.sql
@@ -1,1 +1,22 @@
+/*=====================================================================================
+QUERY: Promotion & Feature Impact - Markdown Effect on Sales
+PURPOSE:
+    - Assess the impact of markdown promotions on weekly sales.
+    - Compare sales performance between weeks with markdowns applied vs. weeks without.
+    - Provide insights into promotion effectiveness for business strategy.
+=====================================================================================*/
+
+SELECT 
+    CASE 
+        WHEN mark_down1 > 0 THEN 'Markdown'      -- Weeks with active markdown promotions
+        ELSE 'No Markdown'                       -- Weeks without markdowns
+    END AS markdown_flag,
+    AVG(weekly_sales) AS avg_sales               -- Average sales for each category
+FROM gold.train_with_stores_and_features
+GROUP BY 
+    CASE 
+        WHEN mark_down1 > 0 THEN 'Markdown' 
+        ELSE 'No Markdown' 
+    END;
+
 
