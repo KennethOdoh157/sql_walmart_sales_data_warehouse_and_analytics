fffaa4c13f02061b29ae01b9116abccbeb9bc1c8
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:05:58 +0100

    Update 02_holiday_sales_impact.sql

--- scripts/02_advanced_analytics/02_holiday_sales_impact.sql
+++ scripts/02_advanced_analytics/02_holiday_sales_impact.sql
@@ -1,1 +1,15 @@
+/*=====================================================================================
+QUERY: Holiday Impact Analysis on Weekly Sales
+PURPOSE:
+    - Compare sales performance between holiday and non-holiday periods.
+    - Measure the effect of holidays on consumer spending behavior.
+    - Provide insights into whether holidays drive higher or lower sales volumes.
+=====================================================================================*/
+
+SELECT 
+    train_is_holiday,                      -- Holiday flag (1 = holiday, 0 = non-holiday)
+    AVG(weekly_sales) AS avg_sales,        -- Average weekly sales for each category
+    SUM(weekly_sales) AS total_sales       -- Total weekly sales for each category
+FROM gold.train_with_stores
+GROUP BY train_is_holiday;                 -- Separate results by holiday vs non-holiday
 
