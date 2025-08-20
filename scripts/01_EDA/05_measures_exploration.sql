78ca68527f7a128f5e794632ac8254435bed9b79
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:01:27 +0100

    Update 05_measures_exploration.sql

--- scripts/01_EDA/05_measures_exploration.sql
+++ scripts/01_EDA/05_measures_exploration.sql
@@ -1,1 +1,18 @@
+/*=====================================================================================
+QUERY: Measures Exploration - Sales and Returns Summary
+PURPOSE:
+    - Generate key summary statistics for sales and returns.
+    - Provide insights into sales distribution, scale, and return behavior.
+    - Acts as an initial data profiling step for business analysis and validation.
+=====================================================================================*/
+
+SELECT 
+    MIN(weekly_sales) AS min_sales,     -- Lowest weekly sales observed
+    MAX(weekly_sales) AS max_sales,     -- Highest weekly sales observed
+    AVG(weekly_sales) AS avg_sales,     -- Average weekly sales across all records
+    SUM(weekly_sales) AS total_sales,   -- Total sales across the dataset
+    AVG(returns) AS avg_returns         -- Average returns across all records
+FROM gold.train_with_stores_and_features;
+
+
 
