da9274a1be03842e2e9d7d6cc8473e54e1b74db5
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:02:59 +0100

    Update 07_top_department_by_avg_sales.sql

--- scripts/01_EDA/07_top_department_by_avg_sales.sql
+++ scripts/01_EDA/07_top_department_by_avg_sales.sql
@@ -1,1 +1,17 @@
+/*=====================================================================================
+QUERY: Top 5 Departments by Average Weekly Sales
+PURPOSE:
+    - Identify which departments generate the highest average weekly sales.
+    - Provide insights into product/service categories with the strongest performance.
+    - Support business decisions on resource allocation, promotions, and inventory planning.
+=====================================================================================*/
+
+SELECT TOP 5 
+    department, 
+    AVG(weekly_sales) AS avg_sales   -- Average sales per department
+FROM gold.train_with_stores_and_features
+GROUP BY department
+ORDER BY avg_sales DESC;             -- Rank departments by average weekly sales
+
+
 
