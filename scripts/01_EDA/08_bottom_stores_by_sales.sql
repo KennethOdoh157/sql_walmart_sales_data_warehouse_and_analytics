b7c7fcf60396703db1750ec6abaebcf7f0d5e76f
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:03:46 +0100

    Update 08_bottom_stores_by_sales.sql

--- scripts/01_EDA/08_bottom_stores_by_sales.sql
+++ scripts/01_EDA/08_bottom_stores_by_sales.sql
@@ -1,1 +1,17 @@
+/*=====================================================================================
+QUERY: Bottom 5 Stores by Total Sales
+PURPOSE:
+    - Identify the lowest-performing stores based on total sales.
+    - Highlight underperforming locations that may require business review.
+    - Useful for performance benchmarking and strategic planning.
+=====================================================================================*/
+
+SELECT TOP 5 
+    store_id, 
+    SUM(weekly_sales) AS total_sales   -- Aggregate sales per store
+FROM gold.train_with_stores_and_features
+GROUP BY store_id
+ORDER BY total_sales ASC;             -- Rank stores by total sales, lowest first
+
+
 
