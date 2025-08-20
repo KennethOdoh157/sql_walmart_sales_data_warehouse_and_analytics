0b39686e42e7c52b71a5a7487cd4a0a32f4519e6
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 15:58:53 +0100

    Update 03_dimension_exploration.sql

--- scripts/01_EDA/03_dimension_exploration.sql
+++ scripts/01_EDA/03_dimension_exploration.sql
@@ -1,1 +1,25 @@
 
+/*=====================================================================================
+QUERY: Dimension Exploration of Stores and Departments
+PURPOSE:
+    - Explore the structure of the dataset by analyzing distinct stores and departments.
+    - Identify how many unique stores exist per store type.
+    - Identify how many stores carry each department.
+    - Supports dimensional understanding of the dataset for reporting and analysis.
+=====================================================================================*/
+
+-- Count unique stores grouped by store type
+SELECT 
+    store_type, 
+    COUNT(DISTINCT store_id) AS num_stores
+FROM gold.train_with_stores
+GROUP BY store_type;
+
+-- Count how many unique stores carry each department
+SELECT 
+    department, 
+    COUNT(DISTINCT store_id) AS stores_per_dept
+FROM gold.train_with_stores
+GROUP BY department;
+
+
