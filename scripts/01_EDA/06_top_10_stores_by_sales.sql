5717d29757744e27f432eeebe4772091e5d484f5
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:02:13 +0100

    Update 06_top_10_stores_by_sales.sql

--- scripts/01_EDA/06_top_10_stores_by_sales.sql
+++ scripts/01_EDA/06_top_10_stores_by_sales.sql
@@ -1,1 +1,16 @@
+/*=====================================================================================
+QUERY: Top 10 Stores by Total Sales
+PURPOSE:
+    - Identify the highest-performing stores based on total sales.
+    - Provide insights into which store locations contribute the most revenue.
+    - Useful for ranking stores and highlighting top performers for business strategy.
+=====================================================================================*/
+
+SELECT TOP 10 
+    store_id, 
+    SUM(weekly_sales) AS total_sales   -- Aggregate sales per store
+FROM gold.train_with_stores_and_features
+GROUP BY store_id
+ORDER BY total_sales DESC;            -- Rank stores by total sales, highest first
+
 
