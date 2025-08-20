02401868bee926f8e05ae96c0dba3974c97ea0db
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 15:59:56 +0100

    Update 04_data_coverage.sql

--- scripts/01_EDA/04_data_coverage.sql
+++ scripts/01_EDA/04_data_coverage.sql
@@ -1,1 +1,16 @@
+/*=====================================================================================
+QUERY: Date Coverage of Training Data
+PURPOSE:
+    - Identify the overall time span of the dataset.
+    - Useful for validating completeness of historical data and understanding 
+      the range of available records.
+=====================================================================================*/
+
+-- Get the earliest and latest dates in the dataset
+SELECT 
+    MIN(date) AS start_date,   -- Earliest available record
+    MAX(date) AS end_date      -- Latest available record
+FROM gold.train_with_stores;
+
+
 
