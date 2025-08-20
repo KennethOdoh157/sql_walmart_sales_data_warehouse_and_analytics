0fc7eca214279a3fde1939b96737d1083330a15f
KennethOdoh157 <kennybrytex@gmail.com>
2025-08-20 16:05:06 +0100

    Update 01_weekly_sales_trend_by_store_type.sql

--- scripts/02_advanced_analytics/01_weekly_sales_trend_by_store_type.sql
+++ scripts/02_advanced_analytics/01_weekly_sales_trend_by_store_type.sql
@@ -1,1 +1,21 @@
+/*=====================================================================================
+QUERY: Weekly Sales Trend by Store Type
+PURPOSE:
+    - Analyze weekly sales patterns across different store types.
+    - Provide visibility into seasonality, demand fluctuations, and performance trends.
+    - Support time-series reporting and advanced analytics for decision-making.
+=====================================================================================*/
+
+SELECT 
+    store_type, 
+    DATEPART(week, date) AS week_num,       -- Extract week number from the date
+    SUM(weekly_sales) AS weekly_sales       -- Total weekly sales per store type
+FROM gold.train_with_stores_and_features
+GROUP BY 
+    store_type, 
+    DATEPART(week, date)                    -- Group by store type and week number
+ORDER BY 
+    store_type, 
+    week_num;                               -- Chronological ordering within each store type
+
 
